bd151df932c82cf5f313b818c0667d8d9eb38129
ijieun <najee0610@naver.com>
2024-02-28 23:21:52 +0900

    글 작성 기능

--- src/app/create/page.js
+++ src/app/create/page.js
@@ -1,3 +1,41 @@
+"use client";
+// 13버전은 next/navigation 에서 가져오기
+import { useRouter } from "next/navigation";
+
 export default function Create() {
-  return <>Create!!!</>;
+  const router = useRouter();
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        const title = e.target.title.value;
+        const body = e.target.body.value;
+        // 옵션 설정
+        const options = {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ title, body }),
+        };
+        // 서버와 통신
+        fetch(`http://localhost:9999/topics`, options)
+          .then((res) => res.json())
+          .then((result) => {
+            console.log(result);
+            const lastid = result.id;
+            router.push(`/read/${lastid}`);
+          });
+      }}>
+      <p>
+        <input type="text" name="title" placeholder="title"></input>
+      </p>
+      <p>
+        <textarea name="body" placeholder="body"></textarea>
+      </p>
+      <p>
+        <input type="submit" value="create"></input>
+      </p>
+    </form>
+  );
 }
